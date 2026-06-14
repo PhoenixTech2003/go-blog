90939f1982a540237a2709de5b6a489cd46c6ca0
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2026-06-14 10:54:38 +0200

    port over the sort and filter

--- internal/web/index_handler.go
+++ internal/web/index_handler.go
@@ -7,14 +7,9 @@ import (
 	"github.com/PhoenixTech2003/go-blog/internal/web/ui/pages"
 )
 
-type errResponseBody struct {
-	Data    any    `json:"data"`
-	Message string `json:"message"`
-}
-
 func (cfg *WebCfg) HandleIndex(w http.ResponseWriter, r *http.Request) {
 
-	component := pages.Index("hello world")
+	component := pages.Index()
 	err := component.Render(r.Context(), w)
 	if err != nil {
 		slog.Error("failed to load index page", "error", err.Error())
