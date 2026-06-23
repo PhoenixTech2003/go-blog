cf6f97c1819f106218af38e6d3921f1c9cc3de88
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2026-06-23 21:00:19 +0200

    Add the new post button

--- clients/go_blog_app/lib/screens/index_screen.dart
+++ clients/go_blog_app/lib/screens/index_screen.dart
@@ -6,7 +6,26 @@ class IndexScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Minimalist")),
+      appBar: AppBar(
+        title: Text("Minimalist"),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8),
+            child: TextButton.icon(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                backgroundColor: Theme.of(context).primaryColor,
+                foregroundColor: Theme.of(context).cardColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0),
+                ),
+              ),
+              label: const Text("New Post"),
+              icon: const Icon(Icons.add),
+            ),
+          ),
+        ],
+      ),
       body: Center(child: Text("Hello world")),
     );
   }
